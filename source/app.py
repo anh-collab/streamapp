0c87e7f9c319880d817540627499319b1f047a9c
Anh Chu <48894658+anh-collab@users.noreply.github.com>
2021-05-12 14:21:28 +0700

    Update app.py

--- source/app.py
+++ source/app.py
@@ -1,16 +1,20 @@
+import csv
+import os
+
 import streamlit as st
 import pandas as pd
+
 from support import add, update,visualize,delete
 from db import *
-import csv
-import os 
 
 def main():
     st.title("Visualization App")
-    menu = ["Add","Delete","Update","Visualize"]
-    options = st.sidebar.selectbox("Menu",menu)
+    menu = ["Add", "Delete", "Update", "Visualize"]
+    options = st.sidebar.selectbox("Menu", menu)
+    
     create_table()
-    if options =="Add":
+    
+    if options == "Add":
           add()
     elif options == "Delete":
           delete()
@@ -18,14 +22,18 @@ def main():
           update()       
     elif options == "Visualize":
           visualize()
+            
     #Exporting data into CSV file
     print('Exporting data into CSV file.....')
+    
     cursor = con.cursor()
     cursor.execute("SELECT * from valuestable")
-    with open("value_data.csv","w") as csv_file:
-        csv_writer = csv.writer(csv_file,delimiter="\t")
+    
+    with open("value_data.csv", "w") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter="\t")
         csv_writer.writerow([i[0] for i in cursor.description])
         csv_writer.writerows(cursor)
+        
     dirpath = os.getcwd() + "/value_data.csv"
     print("Data Exported into {}".format(dirpath))
  
